be95b7cb76adde6ab5e3a19353f91b6222253724
SvetaK7 <atevs7@inbox.ru>
2023-04-14 00:03:26 +0300

    fix: thunks add tasks

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -9,7 +9,7 @@ import {
     removeTodolistTC,
     TodolistDomainType, todolistsActions
 } from './todolists-reducer'
-import { addTaskTC, removeTaskTC, TasksStateType, updateTaskTC } from './tasks-reducer'
+import {removeTaskTC, TasksStateType, tasksThunks, updateTaskTC} from './tasks-reducer'
 import { TaskStatuses } from 'api/todolists-api'
 import { Grid, Paper } from '@mui/material'
 import { AddItemForm } from 'components/AddItemForm/AddItemForm'
@@ -46,13 +46,12 @@ export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
     }, [])
 
     const addTask = useCallback(function (title: string, todolistId: string) {
-        const thunk = addTaskTC(title, todolistId)
+        const thunk = tasksThunks.addTask({title, todolistId})
         dispatch(thunk)
     }, [])
 
     const changeStatus = useCallback(function (id: string, status: TaskStatuses, todolistId: string) {
-        const thunk = updateTaskTC(id, {status}, todolistId)
-        dispatch(thunk)
+        dispatch(updateTaskTC(id, {status}, todolistId))
     }, [])
 
     const changeTaskTitle = useCallback(function (id: string, newTitle: string, todolistId: string) {
--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -1,4 +1,11 @@
-import {TaskPriorities, TaskStatuses, TaskType, todolistsAPI, UpdateTaskModelType} from 'api/todolists-api'
+import {
+  AddTaskArgType,
+  TaskPriorities,
+  TaskStatuses,
+  TaskType,
+  todolistsAPI,
+  UpdateTaskModelType
+} from 'api/todolists-api'
 import {AppThunk} from 'app/store'
 import {appActions} from 'app/app-reducer'
 import {handleServerAppError, handleServerNetworkError} from 'utils/error-utils'
@@ -23,15 +30,14 @@ const fetchTasks = createAppAsyncThunk<{tasks: TaskType[], todolistId: string}, 
   }
 })
 
-const addTask = createAppAsyncThunk<{task: TaskType}, {title: string, todolistId: string}>
+const addTask = createAppAsyncThunk<{task: TaskType}, AddTaskArgType>
 ('tasks/addTask', async (arg, thunkAPI) => {
   const {dispatch, rejectWithValue} = thunkAPI;
   dispatch(appActions.setAppStatus({status: 'loading'}))
-  const res = await todolistsAPI.createTask(arg.todolistId, arg.title)
+  const res = await todolistsAPI.createTask(arg)
     try{
       if (res.data.resultCode === 0) {
         const task = res.data.data.item
-        // dispatch(tasksActions.addTask({task}))
         dispatch(appActions.setAppStatus({status: 'succeeded'}))
         return {task}
       } else {
@@ -45,23 +51,6 @@ const addTask = createAppAsyncThunk<{task: TaskType}, {title: string, todolistId
     }
 })
 
-// export const addTaskTC = (title: string, todolistId: string): AppThunk => (dispatch) => {
-//   dispatch(appActions.setAppStatus({status: 'loading'}))
-//   todolistsAPI.createTask(todolistId, title)
-//     .then(res => {
-//       if (res.data.resultCode === 0) {
-//         const task = res.data.data.item
-//         dispatch(tasksActions.addTask({task}))
-//         dispatch(appActions.setAppStatus({status: 'succeeded'}))
-//       } else {
-//         handleServerAppError(res.data, dispatch);
-//       }
-//     })
-//     .catch((error) => {
-//       handleServerNetworkError(error, dispatch)
-//     })
-// }
-
 
 const initialState: TasksStateType = {}
 
@@ -74,10 +63,6 @@ const slice = createSlice({
       const index = tasks.findIndex(t => t.id === action.payload.taskId)
       if (index !== -1) tasks.splice(index, 1)
     },
-    // addTask: (state, action: PayloadAction<{ task: TaskType }>) => {
-    //   const tasks = state[action.payload.task.todoListId];
-    //   tasks.unshift(action.payload.task)
-    // },
     updateTask: (state, action: PayloadAction<{ taskId: string, model: UpdateDomainTaskModelType, todolistId: string }>) => {
       const tasks = state[action.payload.todolistId];
       const index = tasks.findIndex(t => t.id === action.payload.taskId);
